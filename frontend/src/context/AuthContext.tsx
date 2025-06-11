ede2acbf8d49f8b527112d54b28110156eb1f81d
Jean Marcos <jean.silva552@gmail.com>
2025-06-11 11:13:09 -0300

    feat: config fron

---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,35 @@
+import { createContext, useState, type ReactNode } from "react";
+
+type AuthContextType = {
+  token: string | null;
+  login: (newToken: string) => void;
+  logout: () => void;
+};
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+type AuthProviderProps = {
+  children: ReactNode;
+};
+
+function AuthProvider({ children }: AuthProviderProps) {
+  const [token, setToken] = useState<string | null>(null);
+
+  const login = (newToken: string) => {
+    setToken(newToken);
+    localStorage.setItem("token", newToken);
+  };
+
+  const logout = () => {
+    setToken(null);
+    localStorage.removeItem("token");
+  };
+
+  return (
+    <AuthContext.Provider value={{ token, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export { AuthProvider, AuthContext };
